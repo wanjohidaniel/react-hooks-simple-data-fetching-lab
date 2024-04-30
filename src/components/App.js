5381e0fb65316f5c5c7f1a43cc32d057f06069c1
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-04-30 17:16:32 +0300

    React Simple Data Fetching Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogPicture, setDogPicture] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogPicture(data.message);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching dog picture:", error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogPicture} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
